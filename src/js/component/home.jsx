5227010ff7fdbe4fd4903035d943da742eacd477
AlexGilFdez <alexgilfdez@gmail.com>
2024-11-20 11:11:00 +0000

    To Do List
    1. Cree dos Hooks, uno para las nuevas tareas y otro para la lista de tareas.
    2. Cree una variable para poder añadir las tareas.
    3. Cree otra variable para poder borrar las tareas añadidas.
    4. Hice la estructura del formulario.
    5. Invoqué los Hooks dentro de las funciones evento para que se pudieran añadir las tareas al hacer “Enter” y al hacer click en “borrar”.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [newTodo, setNewTodo] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = () => {
+		setTodos([...todos, newTodo]);
+		setNewTodo("");
+	};
+
+	const deleteTodo = (index) => {
+		const updatedTodos = [...todos];
+		updatedTodos.splice(index, 1);
+		setTodos(updatedTodos);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<h1 className="text-center mt-5">To Do List</h1>
+			<input
+				className="form-control"
+				value={newTodo}
+				placeholder="Añade tu tarea aquí..."
+				onChange={(e) => setNewTodo(e.target.value)}
+				onKeyDown={(e) => {
+					if (e.key === "Enter") {
+						addTodo();
+					}
+				}}
+			/>
+			<ul className="list-group mt-3">
+				{todos.map((todo, index) => (
+					<li
+						key={index}
+						className="list-group-item d-flex justify-content-between align-items-center"
+					>
+						{todo}
+						<button
+							className="btn btn-sm "
+							onClick={() => deleteTodo(index)}
+							style={{ display: "inline" }}
+						>
+							x
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
